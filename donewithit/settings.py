acdf264d9f317d21e6ceafdd0e2515b21f32de55
enocol <eno_col@yahoo.com>
2025-07-06 23:53:03 +0100

    collect staticfiles

--- donewithit/settings.py
+++ donewithit/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]   
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user-uploaded content)
 # https://docs.djangoproject.com/en/5.2/topics/files/
