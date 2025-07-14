9cc857a7b735fb5d66eab0dbdbf51bd28f042ee3
enocol <eno_col@yahoo.com>
2025-07-14 15:37:00 +0100

    Edit product_list and product_tails pages

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from .models import Category
 def product_list(request):
     categories = Category.objects.all()  # Assuming you want to use categories in the template
     search_query = request.GET.get('search', '')
+    category_filter = request.GET.get('category', None)
 
     if search_query:
         products = Product.objects.filter(
@@ -18,7 +19,10 @@ def product_list(request):
             products = Product.objects.all()  # Return all products if no products found
     else:
         products = Product.objects.all()  # or .none() if you want no default results
-        messages.info(request, "Please enter a search term to find products.")
+
+    if category_filter:
+        products = products.filter(category__name=category_filter)
+       
 
     context = {
         'products': products,
