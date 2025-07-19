b49be5f5c1444858ba5e03a17e60be51d3ad9e50
enocol <eno_col@yahoo.com>
2025-07-19 19:17:23 +0100

    Enable displaying owners products on profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def profile(request):
+
     """
     Render the user profile page.
     """
-    return render(request, 'profiles/profile.html')
+    products = Product.objects.filter(seller=request.user)  # Assuming you want to show products created by the user
+    context = {
+        'products': products,
+    }
+    return render(request, 'profiles/profile.html', context)
