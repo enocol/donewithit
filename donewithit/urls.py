e779e83b35a8970e62af64c1a8c4a58bc93a99e9
enocol <eno_col@yahoo.com>
2025-07-28 14:59:09 +0100

    Fix bug

--- donewithit/urls.py
+++ donewithit/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', include('shoppingCart.urls')),  # Include the shopping cart app URLs
     path('', include('checkout.urls')),  # Include the checkout app URLs
     path('', include('profiles.urls')),  # Include the profiles app URLs
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
