9594ca2ce5b25baff9d36d70595b3aad78cabb0a
enocol <eno_col@yahoo.com>
2025-08-30 21:46:52 +0100

    Fix bug in searching for products

--- products/views.py
+++ products/views.py
@@ -7,9 +7,10 @@ from .forms import MoreProductImageFormSet, ProductForm
 from django.contrib.auth.decorators import login_required
 
 def product_list(request):
-    categories = Category.objects.all()  # Assuming you want to use categories in the template
-    search_query = request.GET.get('search', '')
+    categories = Category.objects.all()  
+    search_query = request.GET.get('search', None)
     category_filter = request.GET.get('category', None)
+    products = Product.objects.all()
     
 
     if search_query:
@@ -17,17 +18,12 @@ def product_list(request):
             Q(product_name__icontains=search_query) |
             Q(description__icontains=search_query)
         )
-        if not products.exists():
-            messages.error(request, "No products found matching your search criteria.")
-            products = Product.objects.all()  # Return all products if no products found
-    else:
-        products = Product.objects.all()  # or .none() if you want no default results
-       
+    if not products.exists():
+        messages.error(request, "No products found matching your search criteria.")
+        products = Product.objects.all()
 
     if category_filter:
         products = products.filter(category__name=category_filter)
-    else:
-        products = Product.objects.all()
 
     context = {
         'products': products,
