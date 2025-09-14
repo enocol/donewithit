a7ec0e2df0fc2e02ad19ff5bf6d7199069f57dea
enocol <eno_col@yahoo.com>
2025-09-14 14:59:25 +0100

    run collectstatic

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -4,3 +4,14 @@
     container.scrollBy({ left: distance, behavior: 'auto' });
   }
 
+ // Hide spinner when page fully loads
+  window.onload = function() {
+    const spinner = document.getElementById("loading-spinner");
+    spinner.style.visibility = "hidden";
+  };
+
+  // Show spinner immediately when page starts loading
+  document.addEventListener("DOMContentLoaded", function() {
+    const spinner = document.getElementById("loading-spinner");
+    spinner.style.visibility = "visible";
+  });
