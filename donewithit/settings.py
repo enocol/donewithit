63b96dc94fc8bb550eb97b4ab1b7bfc51425ef5f
enocol <eno_col@yahoo.com>
2025-09-16 06:05:28 +0100

    Customise allauth templates

--- donewithit/settings.py
+++ donewithit/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vzw4ucy_)8#u1=iq-e_$r&-xv#rbzy5&*y+y$0rmewz7bpar81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', ".herokuapp.com"]
 
