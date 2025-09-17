e4b856516e46a2c6a49816f63e5d81554fe911c8
enocol <eno_col@yahoo.com>
2025-09-17 20:53:03 +0100

    Add featured products field in product module and customised home pqge

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def home(request):
     '''Home page view'''
-    return render(request, 'home/home.html')
+    featured_products = Product.objects.filter(is_featured=True)
+    return render(request, 'home/home.html', {'featured_products': featured_products})
--- products/models.py
+++ products/models.py
@@ -31,6 +31,7 @@ class Category(models.Model):
         default='',
     )
     display_name = models.CharField(max_length=50, blank=True, null=True)
+    
 
     def __str__(self):
         return f"{self.name}"
@@ -48,6 +49,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_featured = models.BooleanField(default=False)
     main_image = CloudinaryField('image', null=True, blank=True) # Using CloudinaryField for image storage
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
